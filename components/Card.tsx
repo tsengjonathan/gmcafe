212eef715d1f9f2d3dfc93469396bf2ea569551a
Jonathan Tseng <me@tsengjonathan.com>
2022-02-28 20:31:25 -0500

    Add external link to cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { Asset, TRAIT } from '../types/asset'
 import Chip from './Chip'
+import { ExternalLinkIcon } from '@heroicons/react/outline'
+import { contract, marketplaceUrl } from '../constants'
+import Link from 'next/link'
 
 
 const Card = ({ name, imageUrl: inputUrl, traits, token, isSpecial }: Asset) => {
@@ -11,20 +14,29 @@ const Card = ({ name, imageUrl: inputUrl, traits, token, isSpecial }: Asset) => 
 
   const { background, clothing, colour, mood, feature, object } = traits
 
+  const externalLink = `${marketplaceUrl}/${contract}/${token}`
+
   return (
     <div className={classNames(
       'flex flex-col items-center overflow-hidden rounded lg:pb-6',
       { 'bg-gray-100 drop-shadow transition-transform hover:scale-105 duration-200 pb-4': isSpecial }
     )}>
-      <div className={classNames(
-        'relative w-38 h-38 lg:w-48 lg:h-48 rounded-full overflow-hidden m-3 mt-5',
-        { 'drop-shadow': isSpecial },
-        { 'scale-110 lg:scale-100 transition-transform lg:hover:scale-110 duration-200': !isSpecial }
-      )}>
-        <Image src={imageUrl} alt={name} layout="fill" objectFit="cover" onError={() => setImageUrl('/fallback.png')}/>
+      <div className="relative">
+        <div className={classNames(
+          'relative w-36 h-36 lg:w-48 lg:h-48 rounded-full overflow-hidden m-3',
+          { 'drop-shadow': isSpecial },
+          { 'scale-110 lg:scale-100 transition-transform lg:hover:scale-110 duration-200': !isSpecial }
+        )}>
+          <Image src={imageUrl} alt={name} layout="fill" objectFit="cover" onError={() => setImageUrl('/fallback.png')}/>
+        </div>
+        <Link href={externalLink} passHref>
+          <a target="_blank">
+            <ExternalLinkIcon className="h-5 w-5 m-1 cursor-pointer absolute top-0 right-0 text-gray-400 hover:text-gray-600 transition-colors" />
+          </a>
+        </Link>
       </div>
       <h3 className="mt-2 text-lg cursor-default px-3">{displayName}</h3>
-      <div className="break-inside-avoid">
+      <div className="break-inside-avoid mt-2 px-2">
         {background.map((trait, idx) => {
           const key = `${token}-${trait}-${idx}`
           return <Chip key={key} name={trait} type={TRAIT.BACKGROUND} />
--- constants/index.ts
+++ constants/index.ts
@@ -5,6 +5,7 @@ const baseUrl = 'https://api.opensea.io/api/v1'
 const contract = '0x495f947276749ce646f68ac8c248420045cb7b5e'
 const fallbackImgUrl = 'https://lh3.googleusercontent.com/h02MVM2uAxjAhQs4MzWM7ZYRo1NCxK59nxIm4GgxBu9lBmm60XPDie8bTZ5ppoqFS6pCYb6b62nIvo8E-8FPW4eztOB3AfZTXMXJaW8'
 const collection = 'goodmorningcafe'
+const marketplaceUrl = 'https://opensea.io/assets'
 
 const fallbackAsset: Asset = {
   name: '',
@@ -27,4 +28,5 @@ export {
   highlands,
   fallbackAsset,
   collection,
+  marketplaceUrl
 }
