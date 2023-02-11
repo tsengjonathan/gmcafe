11492489f1798f2d222963905acadc088a1a283b
Jonathan Tseng <me@tsengjonathan.com>
2023-02-10 20:09:16 -0500

    chore: redirect site to gmcafe.io/traits

--- next.config.js
+++ next.config.js
@@ -6,14 +6,23 @@ module.exports = {
   },
   async rewrites() {
     return [
-        {
-            source: '/js/script.js',
-            destination: 'https://plausible.io/js/plausible.js'
-        },
-        {
-            source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
-            destination: 'https://plausible.io/api/event'
-        }
+      {
+        source: '/js/script.js',
+        destination: 'https://plausible.io/js/plausible.js'
+      },
+      {
+        source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
+        destination: 'https://plausible.io/api/event'
+      }
     ]
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://gmcafe.io/traits',
+        permanent: true
+      }
+    ]
+  }
 }
